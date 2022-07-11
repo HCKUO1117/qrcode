bd938b6b3e0bfd35d77993f23138cc3655e1a475
James <james1117pig@gmail.com>
2022-07-12 00:27:59 +0800

    create qrcode temp

--- lib/screen/create_qrcode_page.dart
+++ lib/screen/create_qrcode_page.dart
@@ -1,5 +1,7 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:qrcode/generated/l10n.dart';
+import 'package:qrcode/utils/utils.dart';
 
 class CreateQrcodePage extends StatefulWidget {
   const CreateQrcodePage({Key? key}) : super(key: key);
@@ -9,22 +11,100 @@ class CreateQrcodePage extends StatefulWidget {
 }
 
 class _CreateQrcodePageState extends State<CreateQrcodePage> {
+  final typeList = <BarcodeType>[
+    BarcodeType.QrCode,
+    BarcodeType.Code128,
+    BarcodeType.CodeEAN13,
+  ];
+
+  @override
+  void initState() {
+    for (var element in BarcodeType.values) {
+      if (element != BarcodeType.QrCode &&
+          element != BarcodeType.Code128 &&
+          element != BarcodeType.CodeEAN13) {
+        typeList.add(element);
+      }
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).buildQrcode),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemCount: typeList.length,
+        itemBuilder: (context, index) {
+          final type = typeList[index];
+          return createQrcodeTitle(type);
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      ),
     );
   }
-}
 
-class CreateQrcodeTitle extends StatelessWidget {
-  const CreateQrcodeTitle({Key? key}) : super(key: key);
+  Widget createQrcodeTitle(BarcodeType type) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: const Color(0xffedf3ff),
+      elevation: 0,
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                children: [
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          type.name,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text('info'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: Image.asset(
+                      Utils.getBarcodeIcon(type),
+                    ),
+                  ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+                  const SizedBox(width: 16),
+                ],
+              ),
+            ),
+            if (Utils.getBarcodePro(type))
+              Image.asset(
+                'assets/icons/pro_logo_icon.png',
+                color: Colors.orangeAccent,
+                width: 20,
+                height: 20,
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/utils.dart
@@ -0,0 +1,125 @@
+import 'package:barcode_widget/barcode_widget.dart';
+
+class Utils{
+  static String getBarcodeIcon(BarcodeType type){
+    switch(type){
+      case BarcodeType.CodeITF16:
+        return 'assets/barcode/ITF 16.png';
+      case BarcodeType.CodeITF14:
+        return 'assets/barcode/ITF 14.png';
+      case BarcodeType.CodeEAN13:
+        return 'assets/barcode/EAN 13.png';
+      case BarcodeType.CodeEAN8:
+        return 'assets/barcode/EAN 8.png';
+      case BarcodeType.CodeEAN5:
+        return 'assets/barcode/EAN 5.png';
+      case BarcodeType.CodeEAN2:
+        return 'assets/barcode/EAN 2.png';
+      case BarcodeType.CodeISBN:
+        return 'assets/barcode/ISBN.png';
+      case BarcodeType.Code39:
+        return 'assets/barcode/CODE 39.png';
+      case BarcodeType.Code93:
+        return 'assets/barcode/CODE 93.png';
+      case BarcodeType.CodeUPCA:
+        return 'assets/barcode/UPC A.png';
+      case BarcodeType.CodeUPCE:
+        return 'assets/barcode/UPC E.png';
+      case BarcodeType.Code128:
+        return 'assets/barcode/CODE 128.png';
+      case BarcodeType.GS128:
+        return 'assets/barcode/GS1 128.png';
+      case BarcodeType.Telepen:
+        return 'assets/barcode/Telepen.png';
+      case BarcodeType.QrCode:
+        return 'assets/barcode/QR-Code.png';
+      case BarcodeType.Codabar:
+        return 'assets/barcode/CODABAR.png';
+      case BarcodeType.PDF417:
+        return 'assets/barcode/PDF417.png';
+      case BarcodeType.DataMatrix:
+        return 'assets/barcode/Data Matrix.png';
+      case BarcodeType.Aztec:
+        return 'assets/barcode/Aztec.png';
+      case BarcodeType.Rm4scc:
+        return 'assets/barcode/RM4SCC.png';
+      case BarcodeType.Itf:
+        return 'assets/barcode/ITF.png';
+    }
+  }
+
+  static String getBarcodeIntro(BarcodeType type){
+    switch(type){
+      case BarcodeType.CodeITF16:
+        return 'assets/barcode/ITF 16.png';
+      case BarcodeType.CodeITF14:
+        return 'assets/barcode/ITF 14.png';
+      case BarcodeType.CodeEAN13:
+        return 'assets/barcode/EAN 13.png';
+      case BarcodeType.CodeEAN8:
+        return 'assets/barcode/EAN 8.png';
+      case BarcodeType.CodeEAN5:
+        return 'assets/barcode/EAN 5.png';
+      case BarcodeType.CodeEAN2:
+        return 'assets/barcode/EAN 2.png';
+      case BarcodeType.CodeISBN:
+        return 'assets/barcode/ISBN.png';
+      case BarcodeType.Code39:
+        return 'assets/barcode/CODE 39.png';
+      case BarcodeType.Code93:
+        return 'assets/barcode/CODE 93.png';
+      case BarcodeType.CodeUPCA:
+        return 'assets/barcode/UPC A.png';
+      case BarcodeType.CodeUPCE:
+        return 'assets/barcode/UPC E.png';
+      case BarcodeType.Code128:
+        return 'assets/barcode/CODE 128.png';
+      case BarcodeType.GS128:
+        return 'assets/barcode/GS1 128.png';
+      case BarcodeType.Telepen:
+        return 'assets/barcode/Telepen.png';
+      case BarcodeType.QrCode:
+        return 'assets/barcode/QR-Code.png';
+      case BarcodeType.Codabar:
+        return 'assets/barcode/CODABAR.png';
+      case BarcodeType.PDF417:
+        return 'assets/barcode/PDF417.png';
+      case BarcodeType.DataMatrix:
+        return 'assets/barcode/Data Matrix.png';
+      case BarcodeType.Aztec:
+        return 'assets/barcode/Aztec.png';
+      case BarcodeType.Rm4scc:
+        return 'assets/barcode/RM4SCC.png';
+      case BarcodeType.Itf:
+        return 'assets/barcode/ITF.png';
+    }
+  }
+
+  static bool getBarcodePro(BarcodeType type){
+    switch(type){
+      case BarcodeType.CodeITF16:
+      case BarcodeType.CodeITF14:
+      case BarcodeType.CodeEAN8:
+      case BarcodeType.CodeEAN5:
+      case BarcodeType.CodeEAN2:
+      case BarcodeType.CodeISBN:
+      case BarcodeType.Code39:
+      case BarcodeType.Code93:
+      case BarcodeType.CodeUPCA:
+      case BarcodeType.CodeUPCE:
+      case BarcodeType.GS128:
+      case BarcodeType.Telepen:
+      case BarcodeType.Codabar:
+      case BarcodeType.PDF417:
+      case BarcodeType.DataMatrix:
+      case BarcodeType.Aztec:
+      case BarcodeType.Rm4scc:
+      case BarcodeType.Itf:
+        return true;
+      case BarcodeType.Code128:
+      case BarcodeType.QrCode:
+      case BarcodeType.CodeEAN13:
+        return false;
+    }
+  }
+}
