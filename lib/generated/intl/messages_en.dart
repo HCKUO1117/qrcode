828222a77329ba6619459b5bc1c6c22a338deb0c
James <james1117pig@gmail.com>
2022-08-03 09:56:08 +0800

    temp

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,117 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "address": MessageLookupByLibrary.simpleMessage("Address"),
+        "allDay": MessageLookupByLibrary.simpleMessage("All Day"),
+        "allowedChar": MessageLookupByLibrary.simpleMessage("Allowed Char"),
+        "bookmark": MessageLookupByLibrary.simpleMessage("BOOKMARK"),
+        "build": MessageLookupByLibrary.simpleMessage("build"),
+        "buildQrcode": MessageLookupByLibrary.simpleMessage("Build Qrcode"),
+        "calendar": MessageLookupByLibrary.simpleMessage("CALENDAR"),
+        "call": MessageLookupByLibrary.simpleMessage("Call"),
+        "canNotOpen": MessageLookupByLibrary.simpleMessage("Cannot open"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "choose": MessageLookupByLibrary.simpleMessage("Choose"),
+        "chooseAll": MessageLookupByLibrary.simpleMessage("Choose All"),
+        "chooseType": MessageLookupByLibrary.simpleMessage("Choose Type"),
+        "city": MessageLookupByLibrary.simpleMessage("City"),
+        "complete": MessageLookupByLibrary.simpleMessage("Complete"),
+        "confirm": MessageLookupByLibrary.simpleMessage("Confirm"),
+        "connectWifi": MessageLookupByLibrary.simpleMessage("Connect Wifi"),
+        "contact": MessageLookupByLibrary.simpleMessage("CONTACT"),
+        "content": MessageLookupByLibrary.simpleMessage("Content"),
+        "coordinate": MessageLookupByLibrary.simpleMessage("coordinate"),
+        "copied": MessageLookupByLibrary.simpleMessage("Copied"),
+        "copy": MessageLookupByLibrary.simpleMessage("Copy"),
+        "country": MessageLookupByLibrary.simpleMessage("Country"),
+        "data": MessageLookupByLibrary.simpleMessage("Data"),
+        "displayName": MessageLookupByLibrary.simpleMessage("Name"),
+        "email": MessageLookupByLibrary.simpleMessage("EMAIL"),
+        "end": MessageLookupByLibrary.simpleMessage("end"),
+        "firstName": MessageLookupByLibrary.simpleMessage("First Name"),
+        "geo": MessageLookupByLibrary.simpleMessage("GEO"),
+        "geoLat": MessageLookupByLibrary.simpleMessage("Latitude"),
+        "geoLon": MessageLookupByLibrary.simpleMessage("Longitude"),
+        "history": MessageLookupByLibrary.simpleMessage("History"),
+        "info": MessageLookupByLibrary.simpleMessage("Information"),
+        "lastName": MessageLookupByLibrary.simpleMessage("Last Name"),
+        "launch": MessageLookupByLibrary.simpleMessage("Execute"),
+        "launchUrl": MessageLookupByLibrary.simpleMessage("Open Link"),
+        "name": MessageLookupByLibrary.simpleMessage("name"),
+        "notShowAgain":
+            MessageLookupByLibrary.simpleMessage("Don\'t show again"),
+        "note": MessageLookupByLibrary.simpleMessage("Note"),
+        "notify": MessageLookupByLibrary.simpleMessage("Reminder"),
+        "openMap": MessageLookupByLibrary.simpleMessage("Open Map"),
+        "openWifi":
+            MessageLookupByLibrary.simpleMessage("Please turn on Wifi first"),
+        "organization": MessageLookupByLibrary.simpleMessage("Organization"),
+        "organizationDepartment":
+            MessageLookupByLibrary.simpleMessage("Organization Department"),
+        "organizationName":
+            MessageLookupByLibrary.simpleMessage("Organization Name"),
+        "organizationTitle":
+            MessageLookupByLibrary.simpleMessage("Organization Title"),
+        "password": MessageLookupByLibrary.simpleMessage("password"),
+        "personalInfo": MessageLookupByLibrary.simpleMessage("Personal Info"),
+        "phone": MessageLookupByLibrary.simpleMessage("PHONE"),
+        "postal": MessageLookupByLibrary.simpleMessage("Postal"),
+        "pro": MessageLookupByLibrary.simpleMessage("Upgrade Pro"),
+        "proHint":
+            MessageLookupByLibrary.simpleMessage("Remove ads, unlock features"),
+        "result": MessageLookupByLibrary.simpleMessage("result"),
+        "safetyNotify": MessageLookupByLibrary.simpleMessage(
+            "Before opening the link, please confirm whether it is a trusted link"),
+        "saveBookmark": MessageLookupByLibrary.simpleMessage("Save Bookmark"),
+        "saveCalendar": MessageLookupByLibrary.simpleMessage("Save Calendar"),
+        "saveContact": MessageLookupByLibrary.simpleMessage("Save Contact"),
+        "saveError": MessageLookupByLibrary.simpleMessage("Save failed"),
+        "saveSuccess":
+            MessageLookupByLibrary.simpleMessage("Save successfully"),
+        "search": MessageLookupByLibrary.simpleMessage("Search"),
+        "sendEmail": MessageLookupByLibrary.simpleMessage("Send Email"),
+        "sendSMS": MessageLookupByLibrary.simpleMessage("Send SMS"),
+        "setting": MessageLookupByLibrary.simpleMessage("Setting"),
+        "sms": MessageLookupByLibrary.simpleMessage("SMS"),
+        "soonOpen": MessageLookupByLibrary.simpleMessage("soon to open"),
+        "start": MessageLookupByLibrary.simpleMessage("Start"),
+        "state": MessageLookupByLibrary.simpleMessage("State"),
+        "street": MessageLookupByLibrary.simpleMessage("Street"),
+        "subject": MessageLookupByLibrary.simpleMessage("Subject"),
+        "text": MessageLookupByLibrary.simpleMessage("TEXT"),
+        "toGooglePlay": MessageLookupByLibrary.simpleMessage("To Google Play"),
+        "total": MessageLookupByLibrary.simpleMessage("Total"),
+        "type": MessageLookupByLibrary.simpleMessage("Type"),
+        "url": MessageLookupByLibrary.simpleMessage("URL"),
+        "website": MessageLookupByLibrary.simpleMessage("Website"),
+        "wifi": MessageLookupByLibrary.simpleMessage("WIFI"),
+        "wrongChar":
+            MessageLookupByLibrary.simpleMessage("Unsupported character"),
+        "wrongFormat": MessageLookupByLibrary.simpleMessage("Format error"),
+        "wrongLength":
+            MessageLookupByLibrary.simpleMessage("Length does not match"),
+        "youHaveNotScan": MessageLookupByLibrary.simpleMessage(
+            "You haven\'t scanned any barcodes")
+      };
+}
---
+++ lib/generated/intl/messages_zh.dart
@@ -0,0 +1,107 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'zh';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "address": MessageLookupByLibrary.simpleMessage("地址"),
+        "allDay": MessageLookupByLibrary.simpleMessage("整天"),
+        "allowedChar": MessageLookupByLibrary.simpleMessage("允許的字元"),
+        "bookmark": MessageLookupByLibrary.simpleMessage("書籤"),
+        "build": MessageLookupByLibrary.simpleMessage("建立"),
+        "buildQrcode": MessageLookupByLibrary.simpleMessage("建立Qrcode"),
+        "calendar": MessageLookupByLibrary.simpleMessage("行事曆"),
+        "call": MessageLookupByLibrary.simpleMessage("通話"),
+        "canNotOpen": MessageLookupByLibrary.simpleMessage("無法開啟"),
+        "cancel": MessageLookupByLibrary.simpleMessage("取消"),
+        "choose": MessageLookupByLibrary.simpleMessage("選取"),
+        "chooseAll": MessageLookupByLibrary.simpleMessage("全選"),
+        "chooseType": MessageLookupByLibrary.simpleMessage("選擇類型"),
+        "city": MessageLookupByLibrary.simpleMessage("鄉鎮市"),
+        "complete": MessageLookupByLibrary.simpleMessage("完成"),
+        "confirm": MessageLookupByLibrary.simpleMessage("確認"),
+        "connectWifi": MessageLookupByLibrary.simpleMessage("連接Wifi"),
+        "contact": MessageLookupByLibrary.simpleMessage("聯絡人"),
+        "content": MessageLookupByLibrary.simpleMessage("內容"),
+        "coordinate": MessageLookupByLibrary.simpleMessage("坐標"),
+        "copied": MessageLookupByLibrary.simpleMessage("已複製"),
+        "copy": MessageLookupByLibrary.simpleMessage("複製"),
+        "country": MessageLookupByLibrary.simpleMessage("國家"),
+        "data": MessageLookupByLibrary.simpleMessage("資料"),
+        "displayName": MessageLookupByLibrary.simpleMessage("姓名"),
+        "email": MessageLookupByLibrary.simpleMessage("電子郵件"),
+        "end": MessageLookupByLibrary.simpleMessage("結束"),
+        "firstName": MessageLookupByLibrary.simpleMessage("名子"),
+        "geo": MessageLookupByLibrary.simpleMessage("座標"),
+        "geoLat": MessageLookupByLibrary.simpleMessage("緯度"),
+        "geoLon": MessageLookupByLibrary.simpleMessage("經度"),
+        "history": MessageLookupByLibrary.simpleMessage("歷史紀錄"),
+        "info": MessageLookupByLibrary.simpleMessage("資訊"),
+        "lastName": MessageLookupByLibrary.simpleMessage("姓氏"),
+        "launch": MessageLookupByLibrary.simpleMessage("執行"),
+        "launchUrl": MessageLookupByLibrary.simpleMessage("開啟連結"),
+        "name": MessageLookupByLibrary.simpleMessage("名稱"),
+        "notShowAgain": MessageLookupByLibrary.simpleMessage("不再顯示"),
+        "note": MessageLookupByLibrary.simpleMessage("備註"),
+        "notify": MessageLookupByLibrary.simpleMessage("提醒"),
+        "openMap": MessageLookupByLibrary.simpleMessage("開啟地圖"),
+        "openWifi": MessageLookupByLibrary.simpleMessage("請先開啟Wifi"),
+        "organization": MessageLookupByLibrary.simpleMessage("組織"),
+        "organizationDepartment": MessageLookupByLibrary.simpleMessage("組織部門"),
+        "organizationName": MessageLookupByLibrary.simpleMessage("組織名稱"),
+        "organizationTitle": MessageLookupByLibrary.simpleMessage("組織職稱"),
+        "password": MessageLookupByLibrary.simpleMessage("密碼"),
+        "personalInfo": MessageLookupByLibrary.simpleMessage("個人資訊"),
+        "phone": MessageLookupByLibrary.simpleMessage("電話"),
+        "postal": MessageLookupByLibrary.simpleMessage("郵遞區號"),
+        "pro": MessageLookupByLibrary.simpleMessage("升級專業版"),
+        "proHint": MessageLookupByLibrary.simpleMessage("移除廣告、解鎖功能"),
+        "result": MessageLookupByLibrary.simpleMessage("結果"),
+        "safetyNotify":
+            MessageLookupByLibrary.simpleMessage("開啟連結前，請先確認是否為信任的連結"),
+        "saveBookmark": MessageLookupByLibrary.simpleMessage("儲存書籤"),
+        "saveCalendar": MessageLookupByLibrary.simpleMessage("儲存日曆"),
+        "saveContact": MessageLookupByLibrary.simpleMessage("儲存聯絡人"),
+        "saveError": MessageLookupByLibrary.simpleMessage("儲存失敗"),
+        "saveSuccess": MessageLookupByLibrary.simpleMessage("儲存成功"),
+        "search": MessageLookupByLibrary.simpleMessage("搜尋"),
+        "sendEmail": MessageLookupByLibrary.simpleMessage("寄送Email"),
+        "sendSMS": MessageLookupByLibrary.simpleMessage("寄送簡訊"),
+        "setting": MessageLookupByLibrary.simpleMessage("設定"),
+        "sms": MessageLookupByLibrary.simpleMessage("簡訊"),
+        "soonOpen": MessageLookupByLibrary.simpleMessage("即將開啟"),
+        "start": MessageLookupByLibrary.simpleMessage("開始"),
+        "state": MessageLookupByLibrary.simpleMessage("縣市"),
+        "street": MessageLookupByLibrary.simpleMessage("街/路"),
+        "subject": MessageLookupByLibrary.simpleMessage("標題"),
+        "text": MessageLookupByLibrary.simpleMessage("文字"),
+        "toGooglePlay": MessageLookupByLibrary.simpleMessage("前往Google商店"),
+        "total": MessageLookupByLibrary.simpleMessage("總共"),
+        "type": MessageLookupByLibrary.simpleMessage("類型"),
+        "url": MessageLookupByLibrary.simpleMessage("連結"),
+        "website": MessageLookupByLibrary.simpleMessage("網站"),
+        "wifi": MessageLookupByLibrary.simpleMessage("WIFI"),
+        "wrongChar": MessageLookupByLibrary.simpleMessage("不支援的字元"),
+        "wrongFormat": MessageLookupByLibrary.simpleMessage("格式錯誤"),
+        "wrongLength": MessageLookupByLibrary.simpleMessage("長度不符"),
+        "youHaveNotScan": MessageLookupByLibrary.simpleMessage("您尚未掃描任何條碼")
+      };
+}
